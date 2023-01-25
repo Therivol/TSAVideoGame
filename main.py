5ab6925316d442fd44183723fa326d5938f8b8f5
Zander <alex@barba.org>
2023-01-24 17:28:58 -0700

    first

---
+++ main.py
@@ -0,0 +1,21 @@
+from gui.Window import Window
+
+from util.Time import Time
+from util.Settings import Settings
+
+
+if __name__ == "__main__":
+    Time.awake()
+    Settings.load()
+
+    window = Window()
+    window.awake()
+    window.start()
+
+    while not window.shouldClose:
+        window.calculate_dt()
+        window.poll_events()
+        window.early_update()
+        window.update()
+        window.late_update()
+        window.draw()
